51d822ec127e162caef549a00993f84b9b8ea264
Aditya Wikara <adityawikara@gmail.com>
2021-04-25 23:42:49 -0400

    edited the quotation requests

--- frontend/assets/js/user.js
+++ frontend/assets/js/user.js
@@ -1,3 +1,16 @@
+// this should fetch data from the vendor-db, but for now it is hard coded
+async function loadUserProfile() {
+    document.getElementById("user-id").value = "testing"
+    document.getElementById("user-email").value = "testing"
+    document.getElementById("first-name").value = "testing"
+    document.getElementById("last-name").value = "testing"
+    document.getElementById("user-street").value = "testing"
+    document.getElementById("user-city").value = "testing"
+    document.getElementById("user-state").value = "testing"
+    document.getElementById("user-zipcode").value = "testing"
+}
+
 async function updateUserProfile() {
     console.log("updating user profile")
+
 }
